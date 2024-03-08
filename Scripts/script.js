28d0fe0b7c583aafbeec426dfd7bd7271b92423f
Jebastin Michael Raj <140824684+JebastinMichaelRaj@users.noreply.github.com>
2024-03-08 15:12:14 +0530

    Add files via upload

---
+++ Scripts/script.js
@@ -0,0 +1,44 @@
+﻿// toggle bar 
+const togglebutton = document.getElementsByClassName("toggle-button")[0];
+const navbarlinks = document.getElementsByClassName("nav-links")[0];
+
+togglebutton.addEventListener("click", () => {
+    navbarlinks.classList.toggle('active')
+})
+
+// nav color change by scrolling.
+
+const navbar = document.querySelector(".navbar");
+const navLink = document.querySelector(".nav-links");
+const toggleButton = document.querySelector(".toggle-button")
+
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+
+    if (scrollPos > 0) {
+        navbar.classList.add("scroll");
+        navLink.classList.add("scroll");
+        toggleButton.classList.add("scroll");
+    }
+    else {
+        navbar.style.backgroundColor = "##2b7a78";
+        navbar.classList.remove("scroll");
+        navLink.classList.remove("scroll");
+        toggleButton.classList.remove("scroll");
+    }
+})
+
+// Download Pdf
+function cvDownload() {
+    const pdfPath = 'ResumePdf/JebastinMichaelRaj_Resume.pdf';
+    const anchor = document.createElement('a');
+
+    anchor.href = pdfPath;
+    anchor.download = 'JebastinMichaelRajResume.pdf';
+    document.body.appendChild(anchor);
+    anchor.click();
+
+    document.body.removeChild(anchor);
+
+    console.log("clicked");
+}
